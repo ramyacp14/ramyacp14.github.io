220edf8ba8b96001fb29e4a65bbf2b21deb061e4
Ramya Chowdary Patchala <111948875+ramyacp14@users.noreply.github.com>
2024-09-18 17:24:15 -0400

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -401,7 +401,7 @@ const projectsHeader = {
   title: "Projects",
   description:
     "My projects makes use of vast variety of latest technology tools. My best experience is to create Data Science projects and deploy them to web applications using cloud infrastructure.",
-  avatar_image_path: "projects_image.svg",
+  avatar_image_path: "Projects_top.png",
 };
 
 
@@ -409,7 +409,7 @@ const projectsHeader = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "animated_ashutosh.png",
+    profile_image_path: "contactme.png",
     description:
       "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, Cloud and Opensource Development.",
   },
