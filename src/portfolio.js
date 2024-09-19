921d415b9b1243745eff02876b12ce5647968323
Ramya Chowdary Patchala <111948875+ramyacp14@users.noreply.github.com>
2024-09-18 22:33:28 -0400

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -420,7 +420,7 @@ const contactPageData = {
     country: "USA",
     region: "New York",
     postalCode: "13210",
-    avatar_image_path: "address_image.svg",
+    avatar_image_path: "blogs_image.png",
   },
 };
 
